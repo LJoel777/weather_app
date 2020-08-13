f55e17671d9536bb50d58600be48f26631341baa
LJoel777 <szaknoel@gmail.com>
2020-08-13 11:54:19 +0200

    starter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Details from "./components/Details";
+import CurrentWeather from "./components/CurrentWeather";
+import WeatherForest from "./components/WeatherForecast";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <CurrentWeather />
+      <WeatherForest />
+      <Details />
+    </div>
+  );
 }
 
 export default App;
