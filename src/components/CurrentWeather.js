7f49d19f7db2a415f7deeb3cf38aa4d35f879c8f
LJoel777 <szaknoel@gmail.com>
2020-08-13 12:46:43 +0200

    added current weather icon

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -4,16 +4,18 @@ import axios from "axios";
 
 const CurrentWeather = () => {
   const apiKey = useContext(ApiKey);
-  const [location, setLocation] = useState("Fót");
+  const [location, setLocation] = useState("Africa");
   const [temp, setTemp] = useState(null);
   const [weather, setWeather] = useState(null);
   const [isLoading, setLoading] = useState(false);
+  const [icon, setIcon] = useState("");
 
   useEffect(() => {
     setLoading(true);
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`).then((res) => {
-      setWeather(res.data.weather.id);
+      setWeather(res.data.weather[0].main);
       setTemp(res.data.main.temp);
+      setIcon(`http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`);
       setLoading(false);
     });
   }, [apiKey, location]);
@@ -27,6 +29,7 @@ const CurrentWeather = () => {
         <h1>{location}</h1>
         <h1>{temp}</h1>
         <h1>{weather}</h1>
+        <img src={icon} alt="icon"></img>
       </div>
     );
   } else content = "Loading...";
