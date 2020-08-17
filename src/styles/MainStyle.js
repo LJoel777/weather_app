b4be1fd75d0e4b8cbbce157b98a76111ed08f05f
werneerm <wmarcos25@gmail.com>
2020-08-17 13:49:41 +0200

    weather backgrounds

--- src/styles/MainStyle.js
+++ src/styles/MainStyle.js
@@ -1,13 +1,27 @@
 import styled from "styled-components";
 import Clouds from "../images/Clouds.jpg";
 import Rain from "../images/Rain.jpg";
+import Clear from "../images/Clear.jpg"
+import Snow from "../images/Snow.jpeg"
+import Mist from "../images/Mist.jpeg"
+
 
 const backgroundDecider = (weather) => {
   switch (weather) {
     case "Clouds":
       return Clouds;
     case "Rain":
+    case "Drizzle":
       return Rain;
+    case "Clear":
+      return Clear;
+    case "Snow":
+      return Snow;
+    case "Mist":
+    case "Smoke":
+    case "Haze":
+    case "Fog":
+      return Mist;
     default:
       return Clouds;
   }
